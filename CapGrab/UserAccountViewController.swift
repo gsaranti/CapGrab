ad19e258f37212fa31e66eb54cb0bf025cf48aed
Your Name <gsaranti@lion.lmu.edu>
2018-02-22 14:29:04 -0800

    User accounts now show the images that user's have uploaded.

--- CapGrab/UserAccountViewController.swift
+++ CapGrab/UserAccountViewController.swift
@@ -5,20 +5,56 @@
 //  Created by George Sarantinos on 2/19/18.
 //  Copyright © 2018 George Sarantinos. All rights reserved.
 //
+//  https://medium.com/yay-its-erica/creating-a-collection-view-swift-3-77da2898bb7c
+//
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import FirebaseStorage
 
-class UserAccountViewController: UIViewController {
+class UserAccountViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    var userID = String()
-    
-    @IBOutlet weak var imageCollection: UICollectionView!
+    var imagePaths = [String]()
+    var imageArray = [UIImage]()
     @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var imageCollection: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as! UserImageViewController
+        cell.userImage.image = imageArray[indexPath.item]
+        return cell
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let storage = Storage.storage()
+        let ref: DatabaseReference!
+        ref = Database.database().reference()
+        let userID = Auth.auth().currentUser?.uid
+        ref.child("users").child(userID!).observeSingleEvent(of: .value, with: { (snapshot) in
+            let value = snapshot.value as? NSDictionary
+            self.imagePaths = (value?["photos"] as! [String])
+            for path in self.imagePaths {
+                let httpsReference = storage.reference(forURL: path)
+                httpsReference.getData(maxSize: 1 * 1024 * 1024, completion: { (data, error) in
+                    if let error = error {
+                        print(error.localizedDescription)
+                    } else {
+                        let currentImage = UIImage(data: data!)
+                        self.imageArray.append(currentImage!)
+                        self.imageCollection.reloadData()
+                    }
+                })
+            }
+        }){ (error) in
+            print(error.localizedDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ CapGrab/UserImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UserImageViewController.swift
+//  CapGrab
+//
+//  Created by George Sarantinos on 2/22/18.
+//  Copyright © 2018 George Sarantinos. All rights reserved.
+//
+
+import UIKit
+
+class UserImageViewController: UICollectionViewCell {
+    
+    @IBOutlet weak var userImage: UIImageView!
+    
+}
